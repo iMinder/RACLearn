ffe7fae5cb4391c0c7b4864bd774d68806aa7a70
iminder <hw.zhao@foxmail.com>
2016-11-17 19:50:08 +0800

    加入两个小样例

--- RACDemo/SimpleDemoViewController.m
+++ RACDemo/SimpleDemoViewController.m
@@ -21,6 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //1. 简单的例子
+//    [self.name.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
+//        NSLog(@"输入内容%@", x);
+//    } completed:^{
+//        
+//    }];
+    
+    //2. 加个filter rac_textSignal -> filter(lenght > 3) ->subcribeNext
+//    RACSignal *nameSignal = self.name.rac_textSignal;
+//    RACSignal *filterUserName = [nameSignal filter:^BOOL(id value) {
+//        NSString *text = value;
+//        return text.length > 3;
+//    }];
+//    [filterUserName subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"输入内容必然 > 3 : %@", x);
+//    } completed:^{
+//        
+//    }];
+    [[self.name.rac_textSignal filter:^BOOL(id value) {
+        NSString *text = value;
+        return text.length > 3;
+    }] subscribeNext:^(NSString * _Nullable x) {
+        NSLog(@"输入内容%@", x);
+    } completed:^{
+        
+    }];
+    
 }
 
 
