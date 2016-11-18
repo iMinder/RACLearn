445b47231a140d5ffb841560d90ed92992ef6243
iminder <hw.zhao@foxmail.com>
2016-11-18 14:50:28 +0800

    comine的简单使用

--- RACDemo/SimpleDemoViewController.m
+++ RACDemo/SimpleDemoViewController.m
@@ -6,6 +6,11 @@
 //  Copyright © 2016年 tencent. All rights reserved.
 //
 
+// 学习网址：https://www.raywenderlich.com/62699/reactivecocoa-tutorial-pt1
+// http://www.cocoachina.com/industry/20140115/7702.html
+// 《Functional reactive programming introduction using ReactiveCocoa》 https://www.gitbook.com/book/kevinhm/functionalreactiveprogrammingonios/details
+
+
 #import "SimpleDemoViewController.h"
 #import <ReactiveObjC/ReactiveObjC.h>
 
@@ -40,17 +45,71 @@
 //    } completed:^{
 //        
 //    }];
-    [[self.name.rac_textSignal filter:^BOOL(id value) {
-        NSString *text = value;
-        return text.length > 3;
-    }] subscribeNext:^(NSString * _Nullable x) {
-        NSLog(@"输入内容%@", x);
+//    [[self.name.rac_textSignal filter:^BOOL(id value) {
+//        NSString *text = value;
+//        return text.length > 3;
+//    }] subscribeNext:^(NSString * _Nullable x) {
+//        NSLog(@"输入内容%@", x);
+//    } completed:^{
+//        
+//    }];
+    
+    //3. map 重新包装对象 nsstring ->number
+//    [[[self.name.rac_textSignal map:^id(NSString *text) {
+//        return @(text.length);
+//    }] filter:^BOOL(NSNumber *num) {
+//        return num.integerValue > 3;
+//    }] subscribeNext:^(id x) {
+//        NSLog(@"返回内容是%@", x);
+//    } completed:^{
+//        
+//    }];
+    [self validStatusSingal];
+}
+
+- (void)validStatusSingal {
+    RACSignal *validUserNameSingal = [self.name.rac_textSignal map:^id(NSString *text) {
+        return @([self isValidUserName:text]);
+    }];
+    
+    RACSignal *validPasswordSingal = [self.password.rac_textSignal map:^id(NSString *passwd) {
+        return @([self isValidPassword:passwd]);
+    }];
+    
+    RAC(self.name, backgroundColor) = [validUserNameSingal map:^id(NSNumber *nameValid) {
+        return [nameValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    RAC(self.password, backgroundColor) = [validPasswordSingal map:^id(NSNumber *passwdValid) {
+        return [passwdValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    //combine
+    [[RACSignal combineLatest:@[validPasswordSingal, validUserNameSingal] reduce:^id _Nonnull(NSNumber *passwdValid, NSNumber *nameValid){
+        return @([passwdValid boolValue] && [nameValid boolValue]);
+    }] subscribeNext:^(NSNumber *signupActive) {
+        self.loginButton.enabled = [signupActive boolValue];
+    } completed:^{
+        
+    }];
+    
+    //login action
+    [[self.loginButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"登录事件");
     } completed:^{
         
     }];
     
 }
 
+
+- (BOOL)isValidUserName:(NSString *)text {
+    return text.length > 3;
+}
+
+- (BOOL)isValidPassword:(NSString *)passwd {
+    return passwd.length > 6;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
